bc92f2e2a38d2ebb05ae36cbefe8c13bc2b2b635
pierre donzel <p.donzel-ext@lefebvre-sarrut.eu>
2018-02-28 10:19:42 +0100

    affichage des properties

--- kwowYourRights.pl
+++ kwowYourRights.pl
@@ -6,16 +6,22 @@ use warnings;
 my $repertoire = @ARGV == 0 ? "." : $ARGV[0];
 my @set = `ls -lR $repertoire`;
 
+my %properties;
+
 foreach my $line(@set) {
 	chomp $line;
 	
 	# drwxr-xr-x 1 drncl 197612   0 Feb  5 10:40 efl-deploy-linux/
 	# que signifie le deuxième mot
 	if ($line =~ m/^[d-]rw.(.)(.).{4}\s\w\s(\w*)\b\s(\w*)\b.*\s\b(.*)$/) {
-		my $flag_readg = $1;
-		my $flag_writeg = $2;
-		my $user = $3;
-		my $file_group = $4;
-		my $file_name = $5;
+		$properties{"flag_readg"} = $1;
+		$properties{"flag_writeg"} = $2;
+		$properties{"user"} = $3;
+		$properties{"file_group"} = $4;
+		$properties{"file_name"} = $5;
 	}
+}
+
+foreach my $key (keys(%properties)) {
+   printf "%-30s : %s\n", $key, $properties{$key};
 }
